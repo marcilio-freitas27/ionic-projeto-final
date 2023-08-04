666f21ddae839ef3a50edd339e9e87c49b48a113
Marcilio Freitas <marcilio@beltsistemas.com.br>
2023-08-04 16:40:52 -0300

    caoacitor config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ionic-projeto-final',
+  appId: 'speak.search.do.ifrn.marcilio',
+  appName: 'Speak to search, Speak to do',
   webDir: 'www',
   server: {
     androidScheme: 'https'
